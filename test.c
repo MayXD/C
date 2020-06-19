3aa6d45fb4dee2fc55ecf2b4dd5ce3d405fe4edb
MayXD <1096may1559629925@gmail.com>
2020-06-19 22:11:46 +0800

    add first project

--- test.c
+++ test.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-	int a = 20;
-	int aaa = 100;
-	char bbb = a;
-	printf("hello,world!\n");
-	printf("%c\n",bbb );
+	system("clear");
 	return 0;
 }
