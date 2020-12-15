fb98ce1ef2f88c07e4ce016bb121744e9b8dec92
May-mac <1096may1559629925@gmail.com>
2020-12-15 22:46:02 +0800

    delete LM

--- test.c
+++ test.c
@@ -1,18 +1,7 @@
-#include<stdio.h>
+#include <stdio.h>
 
-<<<<<<< HEAD
 int main()
 {
 	printf("hello,world!\n");
 	return 0;
 }
-=======
-int main(){
-  char temp[10];
-  printf("input string\n");
-  scanf("%s",temp);
-  for(int i=0;i<10;i++)
-    printf("%c",temp[i]);
-  return 0;
-}
->>>>>>> 1f1ba7f2d4269bedca43f63fa83e970985d48809
